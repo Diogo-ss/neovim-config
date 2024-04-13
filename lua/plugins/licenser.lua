610799ef6cca1576370c3632cd7f094d9a2002e8
Diogo-ss <diogosantosdasilva6@gmail.com>
2024-04-12 21:18:58 -0300

    fix: name of licenser

--- lua/plugins/licenser.lua
+++ lua/plugins/licenser.lua
@@ -2,7 +2,7 @@ return {
   "Diogo-ss/licenser.nvim",
   cmd = { "Licenser" },
   opts = {
-    name = "Diogo",
+    name = "Diogo Silva",
   },
   config = function(_, opts)
     require("licenser").setup(opts)
